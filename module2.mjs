75938309b0a9ea17c5d0b163d59da0271b39bc3e
Mutallib <qmutallib2002@gmail.com>
2023-02-10 20:47:28 +0530

    updated project

--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Mutallib";
-const b = "Faiz";
-const c = "Ayan";
-const d = "Arun";
-
-export default b;
-export{a}
-export{c}
-export{d}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -39,20 +39,20 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
   <Alert alert={alert}/>
   <div className="container my-3">
-  {/* <Switch>
+  <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-          <Route exact path="/"> */}
+          <Route exact path="/">
            <TextForm showAlert={showAlert} heading = "Enter the text to analyse below" mode={mode} />
-          {/* </Route> */}
-  {/* </Switch> */}
+          </Route>
+  </Switch>
   </div>
-  {/* </Router> */}
+  </Router>
 
   {/* <About/> */}
   
